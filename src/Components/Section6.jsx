95670f9e86053fdce39d7ed6d765be18e99da9f9
Chuma Wakaba <cwakaba73@gmail.com>
2023-07-14 15:14:22 +0200

    Section6

--- src/Components/Section6.jsx
+++ src/Components/Section6.jsx
@@ -2,35 +2,35 @@ import React from 'react'
 
 const Section6 = () => {
   return (
-    <div className='bg-[#C6BEB5] p-7'>
+    <div className='bg-[#C6BEB5] py-7'>
         <div className='flex justify-between gap-3 border-t border-b border-[#1B1B19] py-7 max-w-[95%] mx-auto'>
-            <div className='flex'>
-                <words>
-                    <h3>Site of the day</h3>
-                    <h1 className='font-Canopee text-xl'>Awards</h1>
+            <div className='flex gap-3 w-1/4'>
+                <words className='text-center'>
+                    <h3 className='font-CondMedium text-3xl uppercase font-extralight text-[#3A3834]'>Site of the day</h3>
+                    <h1 className='font-Canopee text-7xl text-[#1C1C1A]'>Awards</h1>
                 </words>
-                <num>9</num>
+                <num className='font-NewLight text-9xl text-[#1C1C1A]'>9</num>
             </div>
-            <div className='flex'>
-                <words>
-                    <h3>Site of the month</h3>
-                    <h1>Winners</h1>
+            <div className='flex gap-3 w-1/4'>
+                <words className='text-center'>
+                    <h3 className='font-CondMedium text-3xl uppercase font-extralight text-[#3A3834]'>Site of the month</h3>
+                    <h1 className='font-Canopee text-7xl text-[#1C1C1A]'>Winners</h1>
                 </words>
-                <num>1</num>
+                <num className='font-NewLight text-9xl text-[#1C1C1A]'>1</num>
             </div>
-            <div className='flex'>
-                <words>
-                    <h3>FWA of the day</h3>
-                    <h1>Awards</h1>
+            <div className='flex gap-3 w-1/4'>
+                <words className='text-center'>
+                    <h3 className='font-CondMedium text-3xl uppercase font-extralight text-[#3A3834]'>FWA of the day</h3>
+                    <h1 className='font-Canopee text-7xl text-[#1C1C1A]'>Awards</h1>
                 </words>
-                <num>6</num>
+                <num className='font-NewLight text-9xl text-[#1C1C1A]'>6</num>
             </div>
-            <div className='flex'>
-                <words>
-                    <h3>Acclaimed</h3>
-                    <h1>Mentions</h1>
+            <div className='flex gap-3 w-1/4'>
+                <words className='text-center'>
+                    <h3 className='font-CondMedium text-3xl uppercase font-extralight text-[#3A3834]'>Acclaimed</h3>
+                    <h1 className='font-Canopee text-7xl text-[#1C1C1A]'>Mentions</h1>
                 </words>
-                <num>8</num>
+                <num className='font-NewLight text-9xl text-[#1C1C1A]'>8</num>
             </div>
         </div>
     </div>
